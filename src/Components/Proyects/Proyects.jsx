81b5c31c2df9f512ec86c0911916bf4e6ebe9358
unknown <cdwenz@gmail.com>
2021-12-18 21:38:17 -0300

    center all

--- src/Components/Proyects/Proyects.jsx
+++ src/Components/Proyects/Proyects.jsx
@@ -11,43 +11,52 @@ export default function Proyects() {
   return (
     <div id="PROYECTOS" className={styles.containerProyects}>
       <h2 className={styles.title}>PROYECTOS</h2>
-        <div className={styles.contentProyects}>
         <div className={styles.grid}>
-					<a href="https://huellitas.vercel.app/home" target="_blank"><figure className={styles.effectzoe}>
-						<img src={huellitas} alt="img25"/>
-						<figcaption>
-							<h2>Huellitas <span>PG</span></h2>
-							<p className={styles.iconlinks}>
-                <a href="https://github.com/Martintribuzio/huellitas-pg" target="_blank"><AiFillGithub /></a>
-								<a href="https://huellitas.vercel.app/home" target="_blank"><IoGlobe /></a>
-							</p>
-							<p className={styles.description}>App/Red social para publicar animales perdidos o en adopcion <br/> Typescript | React/Redux | Material UI | Mongoose | WebSocket | Passport | Nodemailer | MongoDB</p>
-						</figcaption>		
-					</figure>	</a>
-					<a href="https://github.com/cdwenz/PI-food" target="_blank"><figure className={styles.effectzoe}>
-						<img src={PI} alt="img25"/>
-						<figcaption>
-							<h2>Food <span>PI</span></h2>
-							<p className={styles.iconlinks}>
-								<a href="https://github.com/cdwenz/PI-food" target="_blank"><AiFillGithub /></a>
-								<a href="https://wenz-pi-food.vercel.app/" target="_blank"><IoGlobe /></a>
-							</p>
-							<p className={styles.description}>Web para busqueda y creacion de Recetas de cocina. <br/> Javascript | React | Redux | postgreSql + API externa</p>
-						</figcaption>			
-					</figure></a>
-					<a href="https://github.com/cdwenz/Wenz-Weather"  target="_blank"><figure className={styles.effectzoe}>
-						<img src={weather} alt="img25"/>
-						<figcaption>
-							<h2>Weather <span>APP</span></h2>
-							<p className={styles.iconlinks}>
-                <a href="https://github.com/cdwenz/Wenz-Weather"  target="_blank"><AiFillGithub /></a>
-								<a href="https://wenz-weather.herokuapp.com/" target="_blank"><IoGlobe /></a>
-							</p>
-							<p className={styles.description}>App para visualizar clima en tiempo real. <br/> Javascript | React | HTML/CSS | API externa</p>
-						</figcaption>			
-					</figure></a>
+					<div><a href="https://huellitas.vercel.app/home" target="_blank">
+							<figure className={styles.effectzoe}>
+								<img src={huellitas} alt="img25"/>
+								<figcaption>
+									<h2>Huellitas <span>PG</span></h2>
+									<p className={styles.iconlinks}>
+										<a href="https://github.com/Martintribuzio/huellitas-pg" target="_blank"><AiFillGithub /></a>
+										<a href="https://huellitas.vercel.app/home" target="_blank"><IoGlobe /></a>
+									</p>
+									<p className={styles.description}>App/Red social para publicar animales perdidos o en adopcion <br/> Typescript | React/Redux | Material UI | Mongoose | WebSocket | Passport | Nodemailer | MongoDB</p>
+								</figcaption>		
+							</figure>	
+						</a>
+					</div>
+					<div>
+						<a href="https://github.com/cdwenz/PI-food" target="_blank">
+							<figure className={styles.effectzoe}>
+								<img src={PI} alt="img25"/>
+								<figcaption>
+									<h2>Food <span>PI</span></h2>
+									<p className={styles.iconlinks}>
+										<a href="https://github.com/cdwenz/PI-food" target="_blank"><AiFillGithub /></a>
+										<a href="https://wenz-pi-food.vercel.app/" target="_blank"><IoGlobe /></a>
+									</p>
+									<p className={styles.description}>Web para busqueda y creacion de Recetas de cocina. <br/> Javascript | React | Redux | postgreSql + API externa</p>
+								</figcaption>			
+							</figure>
+						</a>
+					</div>
+					<div>
+						<a href="https://github.com/cdwenz/Wenz-Weather"  target="_blank">
+							<figure className={styles.effectzoe}>
+								<img src={weather} alt="img25"/>
+								<figcaption>
+									<h2>Weather <span>APP</span></h2>
+									<p className={styles.iconlinks}>
+										<a href="https://github.com/cdwenz/Wenz-Weather"  target="_blank"><AiFillGithub /></a>
+										<a href="https://wenz-weather.herokuapp.com/" target="_blank"><IoGlobe /></a>
+									</p>
+									<p className={styles.description}>App para visualizar clima en tiempo real. <br/> Javascript | React | HTML/CSS | API externa</p>
+								</figcaption>			
+							</figure>
+						</a>
+					</div>
 				</div>
-            </div>
     </div>
   );
 }
